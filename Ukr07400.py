2e65d8ac0a05c44bcc83e3b6c48e769de2fd24c0
Burimov <denysburimov@gmail.com>
2018-01-15 16:26:38 +0200

    Отрезаются 32-е числа

--- Ukr07400.py
+++ Ukr07400.py
@@ -10,7 +10,8 @@ def create_calendar_page(month, year, day=1):
     dt_28 = datetime.timedelta(28)
     dt_29 = datetime.timedelta(29)
     dt_30 = datetime.timedelta(30)
-    deltas = [dt_28, dt_29, dt_30]
+    dt_31 = datetime.timedelta(31)
+    deltas = [dt_28, dt_29, dt_30, dt_31]
 
     row_01 = '-' * 20
     row_02 = "MO TU WE TH FR SA SU"
@@ -28,19 +29,35 @@ def create_calendar_page(month, year, day=1):
 
     for i in range(len(deltas)):
         is_day_next_month = today_num + deltas[i]
-        last_day_of_the_month = today_num - datetime.timedelta(1)
+        last_day_of_the_month = today_num + deltas[i] - datetime.timedelta(1)
         if is_day_next_month.day == 1:
-            print last_day_of_the_month.day
+            break
+
+    for i in range(len(days)):
+        if days[i] > last_day_of_the_month.day:
+            days[i] = 0
+
+    days_char = []
+    for i in days:
+        if i < 10:
+            days_char_item = '0' + str(i)
+            days_char.append(days_char_item)
+        else:
+            days_char.append(str(i))
 
 
+    print last_day_of_the_month.day
     print row_01
     print row_02
     print row_03
     print days
+
+    print days_char
+
     print day_name_01
     print today_weekday
 
     return today_str
 
 
-print create_calendar_page(2, 2018)
+print create_calendar_page(6, 2018)
